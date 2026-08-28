77301aaa69d4f4f880b56fcfe37c5fce1bf22d92
Prince Vishwakarma <princevishwakarma122024@gmail.com>
2026-08-28 16:40:16 +0530

    images link convert (ibb to cloudinary

--- src/components/footer.py
+++ src/components/footer.py
@@ -5,7 +5,7 @@ def footer_home():
     st.markdown(f"""
         <div style='margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center;'>
             <p style='font-weight:bold; color:white'/>Created with ❤️ by </p>
-            <img style="width:100%; max-width:100px; height:auto; margin-bottom:10px;" src="https://i.ibb.co/HTTd1q29/logo-bitofprince.png" alt="BITOFPRINCE">
+            <img style="width:100%; max-width:100px; height:auto; margin-bottom:10px;" src="https://res.cloudinary.com/frtasp0i/image/upload/v1787914019/logo_bitofprince.png" alt="BITOFPRINCE">
         </div>
 
                 """
@@ -17,7 +17,7 @@ def footer_dashboard():
     st.markdown(f"""
         <div style='margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center;'>
             <p style='font-weight:bold; color:black'/>Created with ❤️ by </p>
-            <img style="width:100%; max-width:100px; height:auto; margin-bottom:10px;" src="https://i.ibb.co/HTTd1q29/logo-bitofprince.png" alt="BITOFPRINCE">
+            <img style="width:100%; max-width:100px; height:auto; margin-bottom:10px;" src="https://res.cloudinary.com/frtasp0i/image/upload/v1787914019/logo_bitofprince.png" alt="BITOFPRINCE">
         </div>
 
                 """
--- src/components/header.py
+++ src/components/header.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def header_home():
 
-    logo_url= "https://i.ibb.co/YB5JXxP8/logo-Smart-Attend.png"
+    logo_url= "https://res.cloudinary.com/frtasp0i/image/upload/v1787914103/logo_Smart_Attend.png"
 
     st.markdown(f"""
         <div style='display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:10px; margin-top:30px'>
@@ -18,7 +18,7 @@ def header_home():
     
 def header_dashboard():
 
-    logo_url= "https://i.ibb.co/YB5JXxP8/logo-Smart-Attend.png"
+    logo_url= "https://res.cloudinary.com/frtasp0i/image/upload/v1787914103/logo_Smart_Attend.png"
 
     st.markdown(f"""
         <div style='display:flex; align-items:center; justify-content:center; gap:10px;'>
