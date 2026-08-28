a19483bfceba8d2eebc4196c2e1065c9fb072fed
Prince Vishwakarma <princevishwakarma122024@gmail.com>
2026-08-28 15:24:53 +0530

    Finishing Touches

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from src.screens.student_screen import student_screen
 from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
-
+    st.set_page_config(
+        page_title='SyncClass - AI Powered Attendance',
+        page_icon="https://i.ibb.co/YB5JXxP8/logo-Smart-Attend.png"
+    )
 
     if 'login_type' not in st.session_state:
         st.session_state['login_type']= None
